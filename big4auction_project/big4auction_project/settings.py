2ef477b38cdf5ef01ba0c1983bac15b9ed8aad61
Irvine Sunday <amugumbiirvine@gmail.com>
2024-01-22 15:58:53 +0300

    Installed mysqlclient, updated database settings to connect mysql db, migrated changes

--- big4auction_project/big4auction_project/settings.py
+++ big4auction_project/big4auction_project/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'big4auction_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'auction',
+        'USER': 'root',
+        'PASSWORD': 'I$a80899819',
+        'PORT': 3306,
+        'HOST': '127.0.0.1'
     }
 }
 
