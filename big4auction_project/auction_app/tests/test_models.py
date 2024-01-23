ddd71ff6d510b362ee4f49d5671ea2a639c15b79
Irvine Sunday <amugumbiirvine@gmail.com>
2024-01-23 22:15:01 +0300

    created the test directory and the test_models.py

---
+++ big4auction_project/auction_app/tests/test_models.py
@@ -0,0 +1,3 @@
+from django.test import TestCase
+from django.utils import timezone
+from models import Item, ItemImage, Bid, Transaction, Notification, Feedback, Report, PaymentMethod, Category, User
