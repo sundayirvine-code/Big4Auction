f2b4e489f19b377cab072717a4292640fdefa969
Irvine Sunday <amugumbiirvine@gmail.com>
2024-01-24 09:55:04 +0300

    Ensure that each User has a unique phone_number.

--- big4auction_project/auction_app/models.py
+++ big4auction_project/auction_app/models.py
@@ -41,7 +41,7 @@ class User(AbstractUser):
     """
 
     address = models.CharField(max_length=255, null=True, blank=True)
-    phone_number = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=255, unique=True)
 
     def __str__(self):
         return self.username
