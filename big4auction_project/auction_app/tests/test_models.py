e57e9e8401c4da969659abe97b7733aefef4c8d4
Irvine Sunday <amugumbiirvine@gmail.com>
2024-01-24 07:10:04 +0300

    test Item's fields

--- big4auction_project/auction_app/tests/test_models.py
+++ big4auction_project/auction_app/tests/test_models.py
@@ -47,6 +47,20 @@ class ItemModelTest(TestCase):
         )
         self.assertEqual(item.status, 'active')
 
+    def test_item_fields(self):
+        # Test individual fields
+        self.assertEqual(self.item.title, 'Test Item')
+        self.assertEqual(self.item.slug, 'test-item')
+        self.assertEqual(self.item.description, 'This is a test item.')
+        self.assertEqual(self.item.category.category_name, 'Test Category')
+        self.assertEqual(self.item.start_time.day, timezone.now().day)
+        self.assertEqual(self.item.end_time.day, (timezone.now() + timezone.timedelta(days=7)).day)
+        self.assertEqual(self.item.created.day, timezone.now().day)
+        self.assertEqual(self.item.starting_bid, 10.00)
+        self.assertEqual(self.item.reserve_price, 20.00)
+        self.assertEqual(self.item.current_bid, 15.00)
+        self.assertEqual(self.item.status, 'active')
+
     '''def test_item_absolute_url(self):
         expected_url = reverse('item_detail', kwargs={
             'year': self.item.created.year,
