ed2f2e3759e6565f4d6540e83c250ef6cb242719
Irvine Sunday <amugumbiirvine@gmail.com>
2024-01-25 00:44:09 +0300

    include app urls in the project url settings

--- big4auction_project/auction_app/views.py
+++ big4auction_project/auction_app/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from django.views import View
+import stripe
+from django.conf import settings
+from django.http import JsonResponse
 
-# Create your views here.
+stripe.api_key = settings.STRIPE_SECRET_KEY
+
+class CreateCheckoutSessionView(View):
+    def post(self, request, *args, **kwargs):
+        product_id = self.kwargs["pk"]
+        #product = Product.objects.get(id=product_id)
+        YOUR_DOMAIN = "http://127.0.0.1:8000"
+        checkout_session = stripe.checkout.Session.create(
+            payment_method_types=['card'],
+            line_items=[
+                {
+                    'price_data': {
+                        'currency': 'usd',
+                        'unit_amount': 0, #product.price
+                        'product_data': {
+                            'name': '', #product.name
+                            # 'images': ['https://i.imgur.com/EHyR2nP.png'],
+                        },
+                    },
+                    'quantity': 1,
+                },
+            ],
+            metadata={
+                "product_id": 0, #product.id
+            },
+            mode='payment',
+            success_url=YOUR_DOMAIN + '/success/',
+            cancel_url=YOUR_DOMAIN + '/cancel/',
+        )
+        return JsonResponse({
+            'id': checkout_session.id
+        })
