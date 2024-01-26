d45d029a93a9bd754516504d83385677c945fc1d
Irvine Sunday <amugumbiirvine@gmail.com>
2024-01-26 17:07:19 +0300

    wrote appropriate docstrings for the register and login forms

--- big4auction_project/auction_app/forms.py
+++ big4auction_project/auction_app/forms.py
@@ -1,10 +1,16 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.forms.widgets import PasswordInput, TextInput, EmailInput
 from .models import User
-from django.contrib.auth import get_user_model
 
 class RegistrationForm(UserCreationForm):
+    """
+    Custom registration form for user registration.
+
+    Inherits from UserCreationForm and adds an email field.
+
+    Attributes:
+        email (forms.EmailField): Email field for the user.
+    """
     email = forms.EmailField(max_length=255, help_text='Required. Enter a valid email address.')
 
     class Meta:
@@ -12,11 +18,29 @@ class RegistrationForm(UserCreationForm):
         fields = ['email', 'first_name', 'last_name', 'address', 'phone_number']
 
 class LoginForm(AuthenticationForm):
+    """
+    Custom login form for user authentication.
+
+    Inherits from AuthenticationForm and modifies the fields to include an email field.
+
+    Attributes:
+        email (forms.EmailField): Email field for the user.
+    """
     email = forms.EmailField()
+
     class Meta:
         model = User
         fields = ['email', 'password']
 
     def __init__(self, *args, **kwargs):
+        """
+        Initialize the login form.
+
+        Removes the 'username' field from the form.
+
+        Args:
+            *args: Positional arguments.
+            **kwargs: Keyword arguments.
+        """
         super().__init__(*args, **kwargs)
         del self.fields['username']
