110c1f9b5f59d4bc7259c9a2f82c9bf35d96f50c
Irvine Sunday <amugumbiirvine@gmail.com>
2024-01-26 16:56:51 +0300

    made the views dry and added docstrings to each view

--- big4auction_project/auction_app/views.py
+++ big4auction_project/auction_app/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
-import stripe
-from django.conf import settings
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.hashers import make_password, check_password
 from django.http import JsonResponse, HttpResponse
-import json
 from django.views.decorators.csrf import csrf_exempt
-import os
 from .forms import RegistrationForm, LoginForm
 from .models import User
-from django.contrib.auth.hashers import make_password, check_password
 from django.contrib import messages
+import stripe
+import os
+import json
 
+# Retrieve Stripe API keys from environment variables
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
 STRIPE_WEBHOOK_SECRET = os.getenv('STRIPE_WEBHOOK_SECRET')
@@ -18,35 +18,38 @@ stripe.api_key = STRIPE_SECRET_KEY
 
 
 def get_setup_intent_page(request):
+    """Render the page for setting up a payment method."""
     return render(request, 'auction_app/register-card.html')
 
+
 def get_publishable_key(request):
+    """Return the public key for Stripe."""
     return JsonResponse({'publicKey': STRIPE_PUBLIC_KEY})
+
 
 @csrf_exempt
 def create_setup_intent(request):
+    """Create a SetupIntent for setting up a payment method."""
     customer = stripe.Customer.create()
     setup_intent = stripe.SetupIntent.create(customer=customer.id)
     return JsonResponse({
         'client_secret': setup_intent.client_secret,
         'customer': customer.id
-        })
+    })
+
 
 @csrf_exempt
 def webhook_received(request):
-    # You can use webhooks to receive information about asynchronous payment events.
-    # For more about our webhook events check out https://stripe.com/docs/webhooks.
+    """Handle Stripe webhook events."""
     webhook_secret = STRIPE_WEBHOOK_SECRET
     request_data = json.loads(request.body.decode('utf-8'))
-
     payload = request.body
     sig_header = request.META['HTTP_STRIPE_SIGNATURE']
     event = None
-    customer = None
+    customer_id = None
+
     try:
-        event = stripe.Webhook.construct_event(
-            payload, sig_header, settings.STRIPE_WEBHOOK_SECRET
-        )
+        event = stripe.Webhook.construct_event(payload, sig_header, webhook_secret)
     except ValueError as e:
         # Invalid payload
         return HttpResponse(status=400)
@@ -54,26 +57,22 @@ def webhook_received(request):
         # Invalid signature
         return HttpResponse(status=400)
 
-    # Handle the checkout.session.completed event
+    # Handle different Stripe webhook events
     if event['type'] == 'setup_intent.created':
         print('🔔 A new SetupIntent was created.')
-    
-    if event["type"] == "setup_intent.succeeded":
+    elif event["type"] == "setup_intent.succeeded":
         print('🔔 A SetupIntent has successfully set up a PaymentMethod for future use.')
-
-    if event["type"]== 'payment_method.attached':
+    elif event["type"] == 'payment_method.attached':
         print('🔔 A PaymentMethod has successfully been saved to a Customer.')
-        # At this point, redirect to the register page and pass the ID of the Customer object. 
-        # This will be associated with the internal representation of a user.
-
-    if event["type"] == 'setup_intent.setup_failed':
+        customer_id = event['data']['object']['customer']
+    elif event["type"] == 'setup_intent.setup_failed':
         print('🔔 A SetupIntent has failed the attempt to set up a PaymentMethod.')
 
-
-    return redirect('registration', customer_id=customer.id)
+    return redirect('registration', customer_id=customer_id)
 
 
 def registration(request, customer_id):
+    """Handle user registration after setting up a payment method."""
     if not customer_id:
         return HttpResponse("Invalid request. Missing customer_id parameter.")
 
@@ -102,19 +101,9 @@ def registration(request, customer_id):
     context['form'] = form
     return render(request, 'auction_app/registration.html', context)
 
+
 def login_view(request):
-    """
-    View for handling user login.
-
-    If the provided email and password match a user, the user is logged in.
-    Redirects to the login page with an error message if authentication fails.
-
-    Args:
-        request: The HTTP request.
-
-    Returns:
-        HttpResponse: A success message or a redirection to the login page with an error.
-    """
+    """Handle user login."""
     if request.method == 'POST':
         email = request.POST.get('email').strip()
         password = request.POST.get('password').strip()
@@ -128,9 +117,6 @@ def login_view(request):
         if check_password(password, user.password):
             authenticated_user = user
             login(request, authenticated_user)
-
-            # Redirect to a success page or perform other actions
-            print('logged in')
             return HttpResponse('success')
         else:
             messages.error(request, 'Invalid email or password')
